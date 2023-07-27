55dc3a5e7d3ce0903551723f36e20b43171b2c7e
jnordan132 <jacob.nordan113322@gmail.com>
2023-07-27 12:45:02 -0400

    playing around with gallary libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Gallary from "./components/Gallary";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Gallary />
     </>
   );
 }
