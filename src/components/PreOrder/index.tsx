6d097601b7f18594ab793e5b794b2ea2d4b33c11
jnordan132 <jacob.nordan113322@gmail.com>
2023-07-28 12:53:42 -0400

    adding placeholder content for pre-order sectino

--- src/components/PreOrder/index.tsx
+++ src/components/PreOrder/index.tsx
@@ -1,7 +1,30 @@
+import images from "../../gallery.json";
+import "./style.css";
+
 function PreOrder() {
+  console.log(images[2]);
   return (
     <section className="preOrder">
       <h1>Pre-Order</h1>
+      <div className="flex">
+        <div className="shirtPreorder">
+          <img src={images[2].image} />
+        </div>
+        <div className="info">
+          <p>
+            If you would like to pre-order a Dean's Run shirt, Please feel free
+            to contact me personally through either of the below options.
+          </p>
+          <ul>
+            <li>
+              Email: <a href="mailto:blank@gmail.com">blank@gmail.com</a>
+            </li>
+            <li>
+              Phone: <a href="tel:9106228399">(910) 622-8399</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </section>
   );
 }
