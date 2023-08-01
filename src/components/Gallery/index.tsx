19997f210dd1554e46b2fbdf5d6ea5a8bb79e1a8
jnordan132 <jacob.nordan113322@gmail.com>
2023-08-01 09:28:25 -0400

    import updates

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./style.css";
 
 import gallery from "../../gallery.json";
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./style.css";
 import hero from "../../../public/assets/hero2.jpg";
 import brand from "../../../public/assets/brand.jpg";
