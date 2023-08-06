d2040029b7a87e05932c6103898cb7342499cb75
jnordan132 <jacob.nordan113322@gmail.com>
2023-08-06 10:22:10 -0400

    setting up card view and prices

--- src/components/PreOrder/index.tsx
+++ src/components/PreOrder/index.tsx
@@ -3,10 +3,25 @@ import { loadStripe } from "@stripe/stripe-js";
 import shirt from "./shirts.jpg";
 import { useState } from "react";
 
+interface CartItem {
+  price: string;
+  quantity: number;
+}
+
+const itemNames: { [key: string]: string } = {
+  [import.meta.env.VITE_4XL_ID]: "4XL",
+  [import.meta.env.VITE_3XL_ID]: "3XL",
+  [import.meta.env.VITE_2XL_ID]: "2XL",
+  [import.meta.env.VITE_XL_ID]: "XL",
+  [import.meta.env.VITE_L_ID]: "L",
+  [import.meta.env.VITE_M_ID]: "M",
+  [import.meta.env.VITE_S_ID]: "S",
+};
+
 function PreOrder() {
-  console.log(import.meta.env.VITE_TEST);
-  const [stripeError, setStripeError] = useState(null);
+  const [stripeError, setStripeError] = useState<null | string>(null);
   const [isLoading, setLoading] = useState(false);
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
 
   const sizes = ["S", "M", "L", "XL", "2XL", "3XL", "4XL"];
 
@@ -20,75 +35,112 @@ function PreOrder() {
     return stripePromise;
   };
 
-  const items = {
-    price: import.meta.env.VITE_4XL_ID,
-    quantity: 1,
+  const addToCart = (price: string) => {
+    const totalQuantity = cartItems.reduce((total, item) => {
+      return item.price === price ? total + item.quantity : total;
+    }, 0);
+
+    if (totalQuantity === 0) {
+      setCartItems((prevCartItems) => [
+        ...prevCartItems,
+        { price, quantity: 1 },
+      ]);
+    } else {
+      setCartItems((prevCartItems) =>
+        prevCartItems.map((item) =>
+          item.price === price ? { ...item, quantity: item.quantity + 1 } : item
+        )
+      );
+    }
   };
 
-  const checkoutOptions = {
-    lineItems: [items],
-    mode: "payment",
-    successUrl: `${window.location.origin}/success`,
-    cancelUrl: `${window.location.origin}/cancel`,
+  const removeFromCart = (price: string) => {
+    setCartItems((prevCartItems) =>
+      prevCartItems
+        .map((item) =>
+          item.price === price ? { ...item, quantity: item.quantity - 1 } : item
+        )
+        .filter((item) => item.quantity > 0)
+    );
   };
 
   const redirectToCheckout = async () => {
     setLoading(true);
     console.log("redirectToCheckout");
-
     const stripe = await getStripe();
-    const { error } = await stripe.redirectToCheckout(checkoutOptions);
+    const { error } = await stripe.redirectToCheckout({
+      lineItems: cartItems,
+      mode: "payment",
+      successUrl: `${window.location.origin}/success`,
+      cancelUrl: `${window.location.origin}/cancel`,
+    });
     console.log("Stripe checkout error", error);
     if (error) setStripeError(error.message);
     setLoading(false);
   };
 
-  if (stripeError) alert(stripeError);
-
-  // const handleMessage = () => {
-  //   if (`${window.location.origin}/success`) {
-  //     return (
-  //       <div>
-  //         <h3>Success</h3>
-  //         <h4>Thank you for your purchase!</h4>
-  //       </div>
-  //     );
-  //   } else if (`${window.location.origin}/cancel`) {
-  //     return (
-  //       <div>
-  //         <h3>Cancel</h3>
-  //         <h4>Your payment was canceled.</h4>
-  //       </div>
-  //     );
-  //   } else {
-  //     return <div>test</div>;
-  //   }
-  // };
   return (
     <section className="preOrder">
       <h1>Pre-Order</h1>
       <div className="flex">
         <div className="shirtPreorder">
-          <img src={shirt} />
+          <img src={shirt} alt="T-Shirt" />
         </div>
         <div className="info">
           <h4>Deano's Run T-Shirt</h4>
           <b>
-            <h5>$25.00</h5>
+            <h5>$27.00</h5>
           </b>
-          {sizes.map((size) => {
-            return (
+          {sizes.map((size) => (
+            <button
+              key={size}
+              className="checkout-button"
+              onClick={() => addToCart(import.meta.env[`VITE_${size}_ID`])}
+            >
+              {size}
+            </button>
+          ))}
+          <div>
+            <ul>
+              {cartItems.map((item) => (
+                <li key={item.price}>
+                  x{item.quantity} - {itemNames[item.price]}
+                  <button onClick={() => removeFromCart(item.price)}>
+                    Remove
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            {cartItems.length > 0 ? (
               <button
-                className="checkout-button"
+                className="checkoutBtn"
                 onClick={redirectToCheckout}
                 disabled={isLoading}
               >
-                {isLoading ? "Loading..." : size}
+                {isLoading ? "Loading..." : "Checkout"}
               </button>
-            );
-          })}
+            ) : (
+              <></>
+            )}
+          </div>
         </div>
       </div>
+      {`${window.location.pathname}` === "/success" ? (
+        <div>
+          <h3>Success</h3>
+          <h4>Thank you for your purchase!</h4>
+        </div>
+      ) : `${window.location.pathname}` === "/cancel" ? (
+        <div>
+          <h3>Cancel</h3>
+          <h4>Your payment was canceled.</h4>
+        </div>
+      ) : (
+        <div>test</div>
+      )}
+      {stripeError && <div>{stripeError}</div>}
     </section>
   );
 }
