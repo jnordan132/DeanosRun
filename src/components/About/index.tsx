dbd5ca8bf669fda821f7a386a2f65a9a63655431
jnordan132 <jacob.nordan113322@gmail.com>
2023-08-17 16:20:10 -0400

    about me section added

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -5,12 +5,32 @@ function About() {
     <section className="about">
       <h1>About</h1>
       <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
-        voluptates, aspernatur, similique inventore adipisci ipsa dolores
-        voluptatibus sunt rerum assumenda consectetur, fugit rem tempore
-        voluptatum expedita quia. Culpa voluptas voluptatum ea, qui maiores id
-        quaerat alias laudantium ad? Mollitia quibusdam omnis pariatur veniam,
-        porro tempora animi saepe libero. Soluta, ipsa?
+        March 28, 2021 I lost my big brother, Dean, who was struggling severely
+        with his mental health. My brother was my best friend and the most
+        genuine person I have ever met. Always be ready to give advice, listen
+        to your problems, and make you laugh. Though always willing to end a
+        hand or an open ear Dean continued to fight a battle of his own.
+      </p>
+      <p>
+        After such a devastating loss, I have organized this annual motorcycle
+        ride to not only remember Dean but also to raise awareness of the
+        importance of mental health and provide that care to those in our
+        community who cannot afford it.
+      </p>
+      <p>
+        Each ride is led by Dean's motorcycle, followed by tons of close friends
+        and family. Tee shirts can be pre-ordered before the event and picked up
+        daily. Raffle items are donated from dozens of local small businesses,
+        restaurants, bars, etc. in order to raise funds that are donated to
+        Place of Peace counseling in Wilmington, NC. This is a great way to give
+        back to our community in more ways than one.
+      </p>
+      <p>
+        I look forward to making this event bigger and better every year. We
+        have been able to provide mental health care to multiple people in our
+        community over the last few years and would not be able to do so without
+        Place of Peace Counseling as well as all of our other local sponsors.
+        Thank you for your support, and we hope to see you at the next event!
       </p>
     </section>
   );
