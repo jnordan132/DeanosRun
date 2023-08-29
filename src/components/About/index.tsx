41b8227891234456cea48ba83eb96438549eec22
jnordan132 <jacob.nordan113322@gmail.com>
2023-08-29 08:22:08 -0400

    updated about section

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -5,33 +5,49 @@ function About() {
     <section className="about">
       <h1>About</h1>
       <p>
-        March 28, 2021 I lost my big brother, Dean, who was struggling severely
-        with his mental health. My brother was my best friend and the most
-        genuine person I have ever met. Always be ready to give advice, listen
-        to your problems, and make you laugh. Though always willing to end a
-        hand or an open ear Dean continued to fight a battle of his own.
+        <b>
+          On March 28, 2021, I lost my big brother, Dean, who was struggling
+          severely with his mental health.
+        </b>{" "}
+        Dean was mv best triend and the most genuine person i've ever met. He
+        was always ready to offer great advice, listen to your problems, and
+        make those around him laugh. While Dean was quick to lend a hand or an
+        open ear. he was constantly fighting a battle of his own.
       </p>
       <p>
-        After such a devastating loss, I have organized this annual motorcycle
-        ride to not only remember Dean but also to raise awareness of the
-        importance of mental health and provide that care to those in our
-        community who cannot afford it.
+        After such a devastating loss,{" "}
+        <b>
+          I organized this annual motorcycle tribute ride to remember Dean,
+          establish awareness around the importance of mental health, and raise
+          funds to help provide care to those in our community who cannot afford
+          such.
+        </b>
       </p>
       <p>
-        Each ride is led by Dean's motorcycle, followed by tons of close friends
-        and family. Tee shirts can be pre-ordered before the event and picked up
-        daily. Raffle items are donated from dozens of local small businesses,
-        restaurants, bars, etc. in order to raise funds that are donated to
-        Place of Peace counseling in Wilmington, NC. This is a great way to give
-        back to our community in more ways than one.
+        <b>
+          Dean's Run is a great way to help strengthen and support our community
+          in more ways than one.
+        </b>{" "}
+        Each year, the ride is led with Dean's motorcycle, followed by tons of
+        close friends, family, and supporters. A new t-shirt design is created
+        for every event - t-shirts can be preordered before the event and picked
+        up day of. Raffle items are donated from local small businesses,
+        restaurants, bars, etc., to assist in raising monetary donations for
+        Place of Peace Counseling in Wilmington, NC.
       </p>
       <p>
-        I look forward to making this event bigger and better every year. We
-        have been able to provide mental health care to multiple people in our
-        community over the last few years and would not be able to do so without
-        Place of Peace Counseling as well as all of our other local sponsors.
-        Thank you for your support, and we hope to see you at the next event!
+        <b>
+          Through Dean's Run, we have been able to provide mental health care to
+          several community members and hope to continue doing so.
+        </b>{" "}
+        We would not be able to do this without Place Of Peace Counseling and
+        our other local sponsors.
       </p>
+      <p>
+        <b>I look forward to making this event bigger and better every year.</b>{" "}
+        Thank you for your support - we hope to see you at the next Deano's Run!
+      </p>
+      <p className="writtenBy">- Jake Wierzbicki</p>
     </section>
   );
 }
